aa664069103e3d5884beaeb6f8d59995a055ff21
runs <runs@runs-PC.runs.es>
2020-07-28 13:46:13 +0200

    fixes

--- init.lua
+++ init.lua
@@ -3,6 +3,8 @@ local modpath = minetest.get_modpath(modname)
 -- internationalization boilerplate
 local S = minetest.get_translator(minetest.get_current_modname())
 
+-- Register Biomes
+
 minetest.register_biome({
 	name = "swampz",
 	node_top = "swaz:silt_with_grass",
@@ -36,6 +38,8 @@ minetest.register_biome({
 	humidity_point = 68,
 	vertical_blend = 0,
 })
+
+-- Register Nodes
 
 minetest.register_node("swaz:limestone", {
 	description = S("Limestone"),
@@ -175,7 +179,144 @@ minetest.register_node("swaz:water_flowing", {
 	sounds = default.node_sound_water_defaults(),
 })
 
+minetest.register_node("swaz:reed", {
+	description = S("Reed"),
+	drawtype = "plantlike",
+	tiles = {"swaz_reed.png"},
+	inventory_image = "swaz_reed.png",
+	wield_image = "swaz_reed.png",
+	paramtype = "light",
+	sunlight_propagates = true,
+	walkable = false,
+	selection_box = {
+		type = "fixed",
+		fixed = {-1 / 16, -0.5, -1 / 16, 1 / 16, 0.5, 1 / 16},
+	},
+	groups = {snappy = 3, flammable = 2},
+	sounds = default.node_sound_leaves_defaults(),
+
+	after_dig_node = function(pos, node, metadata, digger)
+		default.dig_up(pos, node, digger)
+	end,
+})
+
+minetest.register_node("swaz:swamp_grass", {
+	description = S("Swamp Grass"),
+	drawtype = "plantlike",
+	waving = 1,
+	visual_scale = 0.8,
+	tiles = {"swaz_swamp_grass.png"},
+	inventory_image = "swaz_swamp_grass.png",
+	wield_image = "swaz_swamp_grass.png",
+	paramtype = "light",
+	sunlight_propagates = true,
+	walkable = false,
+	buildable_to = true,
+	groups = {snappy = 3, flora = 1, attached_node = 1, flammable = 1},
+	sounds = default.node_sound_leaves_defaults(),
+	selection_box = {
+		type = "fixed",
+		fixed = {-4 / 16, -0.5, -4 / 16, 4 / 16, 0.0, 4 / 16},
+	},
+})
+
+minetest.register_node("swaz:pampas_grass", {
+	description = S("Pampas Grass"),
+	drawtype = "plantlike",
+	waving = 1,
+	visual_scale = 0.8,
+	tiles = {"swaz_pampas_grass.png"},
+	inventory_image = "swaz_pampas_grass.png",
+	wield_image = "swaz_pampas_grass.png",
+	paramtype = "light",
+	sunlight_propagates = true,
+	walkable = false,
+	buildable_to = true,
+	groups = {snappy = 3, flora = 1, attached_node = 1, flammable = 1},
+	sounds = default.node_sound_leaves_defaults(),
+	selection_box = {
+		type = "fixed",
+		fixed = {-4 / 16, -0.5, -4 / 16, 4 / 16, 0.0, 4 / 16},
+	},
+})
+
+minetest.register_node("swaz:lavender", {
+	description = S("Lavender"),
+	drawtype = "plantlike",
+	waving = 1,
+	visual_scale = 0.8,
+	tiles = {"swaz_lavender.png"},
+	inventory_image = "swaz_lavender.png",
+	wield_image = "swaz_lavender.png",
+	paramtype = "light",
+	sunlight_propagates = true,
+	walkable = false,
+	buildable_to = true,
+	groups = {snappy = 3, flora = 1, attached_node = 1, flammable = 1},
+	sounds = default.node_sound_leaves_defaults(),
+	selection_box = {
+		type = "fixed",
+		fixed = {-2 / 16, -0.5, -2 / 16, 2 / 16, 0, 2 / 16},
+	},
+})
+
+-- Register Decoration
+-- IMPORTANT!
+-- THE ORDER OF THE DECORATION MATTERS!
+-- DO NOT SORT
+
 if mg_name ~= "v6" and mg_name ~= "singlenode" then
+
+	--Bald Cypress
+
+	minetest.register_decoration({
+		deco_type = "schematic",
+		place_on = {"default:dirt", "default:sand", "swaz:silt_with_grass"},
+		sidelen = 16,
+		noise_params = {
+			offset = 0.01,
+			scale = 0.005,
+			spread = {x = 250, y = 250, z = 250},
+			seed = 2,
+			octaves = 3,
+			persist = 0.66
+		},
+		biomes = {"swampz"},
+		height = 2,
+		y_min = 0,
+		y_max = 1000,
+		place_offset_y = 0,
+		schematic = minetest.get_modpath("baldcypress").."/schematics/baldcypress.mts",
+		flags = "place_center_x, place_center_z, force_placement",
+		rotation = "random",
+	})
+
+	--Willow
+
+	minetest.register_decoration({
+		deco_type = "schematic",
+		place_on = {"default:dirt", "default:sand", "swaz:silt_with_grass"},
+		sidelen = 16,
+		noise_params = {
+			offset = 0.01,
+			scale = 0.005,
+			spread = {x = 250, y = 250, z = 250},
+			seed = 2,
+			octaves = 3,
+			persist = 0.66
+		},
+		biomes = {"swampz"},
+		height = 2,
+		y_min = 0,
+		y_max = 1000,
+		place_offset_y = 0,
+		schematic = minetest.get_modpath("willow").."/schematics/willow.mts",
+		flags = "place_center_x, place_center_z, force_placement",
+		rotation = "random",
+	})
+
+	--Water Source (4x4)
+
 	minetest.register_decoration({
 		deco_type = "schematic",
 		place_on = {"swaz:silt_with_grass"},
@@ -207,9 +348,9 @@ if mg_name ~= "v6" and mg_name ~= "singlenode" then
 		flags = "place_center_x, place_center_z, force_placement",
 		rotation = "random",
 	})
-end
 
-if mg_name ~= "v6" and mg_name ~= "singlenode" then
+	--Water Source (1x1) --this goes after the 4x4 deco of swamp water
+
 	minetest.register_decoration({
 		deco_type = "simple",
 		decoration = "swaz:water_source",
@@ -233,141 +374,106 @@ if mg_name ~= "v6" and mg_name ~= "singlenode" then
 		flags = "place_center_x, place_center_z, force_placement",
 		rotation = "random",
 	})
-end
 
---Mud
+	--Mud with moss --this goes after water (depends on it)
 
-minetest.register_decoration({
-	decoration = "swaz:mud_with_moss",
-	deco_type = "simple",
-	place_on = "swaz:silt_with_grass",
-	sidelen = 16,
-	fill_ratio = 0.1,
-	biomes = {"swampz"},
-	noise_params = {
-		offset = 0.5,
-		scale = 0.008,
-		spread = {x = 250, y = 250, z = 250},
-		seed = 2,
-		octaves = 3,
-		persist = 0.66
-	},
-	y_min = 1,
-	y_max = 80,
-	spawn_by = "swaz:water_source",
-	num_spawn_by = 1,
-	place_offset_y = -1,
-	flags = "place_center_x, place_center_z, force_placement",
-})
+	minetest.register_decoration({
+		decoration = "swaz:mud_with_moss",
+		deco_type = "simple",
+		place_on = "swaz:silt_with_grass",
+		sidelen = 16,
+		fill_ratio = 0.1,
+		biomes = {"swampz"},
+		noise_params = {
+			offset = 0.5,
+			scale = 0.008,
+			spread = {x = 250, y = 250, z = 250},
+			seed = 2,
+			octaves = 3,
+			persist = 0.66
+		},
+		y_min = 1,
+		y_max = 80,
+		spawn_by = "swaz:water_source",
+		num_spawn_by = 1,
+		place_offset_y = -1,
+		flags = "place_center_x, place_center_z, force_placement",
+	})
 
-minetest.register_decoration({
-	decoration = "swaz:mud",
-	deco_type = "simple",
-	place_on = "swaz:silt_with_grass",
-	sidelen = 16,
-	fill_ratio = 0.1,
-	biomes = {"swampz"},
-	noise_params = {
-		offset = 0.5,
-		scale = 0.008,
-		spread = {x = 250, y = 250, z = 250},
-		seed = 2,
-		octaves = 3,
-		persist = 0.66
-	},
-	y_min = 1,
-	y_max = 80,
-	spawn_by = "swaz:mud_with_moss",
-	num_spawn_by = 1,
-	place_offset_y = -1,
-	flags = "place_center_x, place_center_z, force_placement",
-})
+	--Mud  --this goes after mud with moss (depends on it)
 
-minetest.register_node("swaz:swamp_grass", {
-	description = S("Swamp Grass"),
-	drawtype = "plantlike",
-	waving = 1,
-	visual_scale = 0.8,
-	tiles = {"swaz_swamp_grass.png"},
-	inventory_image = "swaz_swamp_grass.png",
-	wield_image = "swaz_swamp_grass.png",
-	paramtype = "light",
-	sunlight_propagates = true,
-	walkable = false,
-	buildable_to = true,
-	groups = {snappy = 3, flora = 1, attached_node = 1, flammable = 1},
-	sounds = default.node_sound_leaves_defaults(),
-	selection_box = {
-		type = "fixed",
-		fixed = {-4 / 16, -0.5, -4 / 16, 4 / 16, 0.0, 4 / 16},
-	},
-})
+	minetest.register_decoration({
+		decoration = "swaz:mud",
+		deco_type = "simple",
+		place_on = "swaz:silt_with_grass",
+		sidelen = 16,
+		fill_ratio = 0.1,
+		biomes = {"swampz"},
+		noise_params = {
+			offset = 0.5,
+			scale = 0.008,
+			spread = {x = 250, y = 250, z = 250},
+			seed = 2,
+			octaves = 3,
+			persist = 0.66
+		},
+		y_min = 1,
+		y_max = 80,
+		spawn_by = "swaz:mud_with_moss",
+		num_spawn_by = 1,
+		place_offset_y = -1,
+		flags = "place_center_x, place_center_z, force_placement",
+	})
 
-minetest.register_decoration({
-	decoration = "swaz:swamp_grass",
-	deco_type = "simple",
-	place_on = "swaz:silt_with_grass",
-	sidelen = 16,
-	fill_ratio = 2.0,
-	biomes = {"swampz"},
-	noise_params = {
-		offset = 0.8,
-		scale = 0.08,
-		spread = {x = 250, y = 250, z = 250},
-		seed = 2,
-		octaves = 3,
-		persist = 0.66
-	},
-	y_min = 1,
-	y_max = 80,
-	spawn_by = "swaz:water_source",
-	num_spawn_by = 1,
-	--num_spawn_by = 8,
-})
+	--Swamp Grass --this goes after water (depends on it)
 
-minetest.register_node("swaz:pampas_grass", {
-	description = S("Pampas Grass"),
-	drawtype = "plantlike",
-	waving = 1,
-	visual_scale = 0.8,
-	tiles = {"swaz_pampas_grass.png"},
-	inventory_image = "swaz_pampas_grass.png",
-	wield_image = "swaz_pampas_grass.png",
-	paramtype = "light",
-	sunlight_propagates = true,
-	walkable = false,
-	buildable_to = true,
-	groups = {snappy = 3, flora = 1, attached_node = 1, flammable = 1},
-	sounds = default.node_sound_leaves_defaults(),
-	selection_box = {
-		type = "fixed",
-		fixed = {-4 / 16, -0.5, -4 / 16, 4 / 16, 0.0, 4 / 16},
-	},
-})
+	minetest.register_decoration({
+		decoration = "swaz:swamp_grass",
+		deco_type = "simple",
+		place_on = "swaz:silt_with_grass",
+		sidelen = 16,
+		fill_ratio = 2.0,
+		biomes = {"swampz"},
+		noise_params = {
+			offset = 0.8,
+			scale = 0.08,
+			spread = {x = 250, y = 250, z = 250},
+			seed = 2,
+			octaves = 3,
+			persist = 0.66
+		},
+		y_min = 1,
+		y_max = 80,
+		spawn_by = "swaz:water_source",
+		num_spawn_by = 1,
+		--num_spawn_by = 8,
+	})
 
-minetest.register_decoration({
-	decoration = "swaz:pampas_grass",
-	deco_type = "simple",
-	place_on = "swaz:silt_with_grass",
-	sidelen = 16,
-	fill_ratio = 0.008,
-	biomes = {"swampz"},
-	noise_params = {
-		offset = 0.01,
-		scale = 0.008,
-		spread = {x = 250, y = 250, z = 250},
-		seed = 2,
-		octaves = 3,
-		persist = 0.66
-	},
-	y_min = 1,
-	y_max = 80,
-	spawn_by = "swaz:water_source",
-	num_spawn_by = 1,
-})
+	--Pampa Grass  --this goes after water (depends on it)
 
+	minetest.register_decoration({
+		decoration = "swaz:pampas_grass",
+		deco_type = "simple",
+		place_on = "swaz:silt_with_grass",
+		sidelen = 16,
+		fill_ratio = 0.008,
+		biomes = {"swampz"},
+			noise_params = {
+			offset = 0.01,
+			scale = 0.008,
+			spread = {x = 250, y = 250, z = 250},
+			seed = 2,
+			octaves = 3,
+			persist = 0.66
+		},
+		y_min = 1,
+		y_max = 80,
+		spawn_by = "swaz:water_source",
+		num_spawn_by = 1,
+	})
 
-if mg_name ~= "v6" and mg_name ~= "singlenode" then
+	--Reed --this goes after water (depends on it)
+
 	minetest.register_decoration({
 		deco_type = "schematic",
 		place_on = {"swaz:silt_with_grass", "swaz:mud_with_moss", "swaz:mud"},
@@ -396,163 +502,49 @@ if mg_name ~= "v6" and mg_name ~= "singlenode" then
 		flags = "place_center_x, place_center_z, force_placement",
 		rotation = "random",
 	})
-end
 
-minetest.register_decoration({
-	name = "swaz:waterlily",
-	deco_type = "simple",
-	place_on = {"default:dirt"},
-	sidelen = 16,
-	noise_params = {
-		offset = -0.12,
-		scale = 0.3,
-		spread = {x = 200, y = 200, z = 200},
-		seed = 33,
-		octaves = 3,
-		persist = 0.7
-	},
-	biomes = {"swaz"},
-	y_max = 0,
-	y_min = 0,
-	decoration = "flowers:waterlily_waving",
-	param2 = 0,
-	param2_max = 3,
-	place_offset_y = 1,
-})
+	--Waterlily  --this goes after water (depends on it)
 
-minetest.register_node("swaz:lavender", {
-	description = S("Lavender"),
-	drawtype = "plantlike",
-	waving = 1,
-	visual_scale = 0.8,
-	tiles = {"swaz_lavender.png"},
-	inventory_image = "swaz_lavender.png",
-	wield_image = "swaz_lavender.png",
-	paramtype = "light",
-	sunlight_propagates = true,
-	walkable = false,
-	buildable_to = true,
-	groups = {snappy = 3, flora = 1, attached_node = 1, flammable = 1},
-	sounds = default.node_sound_leaves_defaults(),
-	selection_box = {
-		type = "fixed",
-		fixed = {-2 / 16, -0.5, -2 / 16, 2 / 16, 0, 2 / 16},
-	},
-})
-
-minetest.register_decoration({
-	decoration = "swaz:lavender",
-	deco_type = "simple",
-	place_on = "swaz:silt_with_grass",
-	sidelen = 16,
-	fill_ratio = 0.008,
-	biomes = {"swampz"},
-	sidelen = 16,
-	noise_params = {
-		offset = 0.005,
-		scale = 0.008,
-		spread = {x = 250, y = 250, z = 250},
-		seed = 2,
-		octaves = 3,
-		persist = 0.66
-	},
-	y_min = 1,
-	y_max = 80,
-	--num_spawn_by = 8,
-})
-
--- Waterlily
-
-minetest.register_decoration({
-	decoration = "flowers:waterlily",
-	deco_type = "simple",
-	place_on = "swaz:water_source",
-	sidelen = 16,
-	fill_ratio = 0.008,
-	biomes = {"swampz"},
-	sidelen = 16,
-	noise_params = {
-		offset = 0.05,
-		scale = 0.008,
-		spread = {x = 250, y = 250, z = 250},
-		seed = 2,
-		octaves = 3,
-		persist = 0.66
-	},
-	y_min = 1,
-	y_max = 1000,
-	--num_spawn_by = 8,
-})
-
---Bald Cypress
-
-if mg_name ~= "v6" and mg_name ~= "singlenode" then
 	minetest.register_decoration({
-		deco_type = "schematic",
-		place_on = {"default:dirt", "default:sand", "swaz:silt_with_grass"},
+		decoration = "flowers:waterlily",
+		deco_type = "simple",
+		place_on = "swaz:water_source",
+		sidelen = 16,
+		fill_ratio = 0.008,
+		biomes = {"swampz"},
 		sidelen = 16,
 		noise_params = {
-			offset = 0.01,
-			scale = 0.005,
+			offset = 0.05,
+			scale = 0.008,
 			spread = {x = 250, y = 250, z = 250},
 			seed = 2,
 			octaves = 3,
 			persist = 0.66
 		},
-		biomes = {"swampz"},
-		height = 2,
-		y_min = 0,
+		y_min = 1,
 		y_max = 1000,
-		place_offset_y = 0,
-		schematic = minetest.get_modpath("baldcypress").."/schematics/baldcypress.mts",
-		flags = "place_center_x, place_center_z, force_placement",
-		rotation = "random",
 	})
-end
 
---Willow
+	--Lavender
 
-if mg_name ~= "v6" and mg_name ~= "singlenode" then
 	minetest.register_decoration({
-		deco_type = "schematic",
-		place_on = {"default:dirt", "default:sand", "swaz:silt_with_grass"},
+		decoration = "swaz:lavender",
+		deco_type = "simple",
+		place_on = "swaz:silt_with_grass",
+		sidelen = 16,
+		fill_ratio = 0.008,
+		biomes = {"swampz"},
 		sidelen = 16,
 		noise_params = {
-			offset = 0.01,
-			scale = 0.005,
+			offset = 0.005,
+			scale = 0.008,
 			spread = {x = 250, y = 250, z = 250},
 			seed = 2,
 			octaves = 3,
 			persist = 0.66
 		},
-		biomes = {"swampz"},
-		height = 2,
-		y_min = 0,
-		y_max = 1000,
-		place_offset_y = 0,
-		schematic = minetest.get_modpath("willow").."/schematics/willow.mts",
-		flags = "place_center_x, place_center_z, force_placement",
-		rotation = "random",
+		y_min = 1,
+		y_max = 80,
 	})
+
 end
-
-minetest.register_node("swaz:reed", {
-	description = S("Reed"),
-	drawtype = "plantlike",
-	tiles = {"swaz_reed.png"},
-	inventory_image = "swaz_reed.png",
-	wield_image = "swaz_reed.png",
-	paramtype = "light",
-	sunlight_propagates = true,
-	walkable = false,
-	selection_box = {
-		type = "fixed",
-		fixed = {-1 / 16, -0.5, -1 / 16, 1 / 16, 0.5, 1 / 16},
-	},
-	groups = {snappy = 3, flammable = 2},
-	sounds = default.node_sound_leaves_defaults(),
-
-	after_dig_node = function(pos, node, metadata, digger)
-		default.dig_up(pos, node, digger)
-	end,
-})
